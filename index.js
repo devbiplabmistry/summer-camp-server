cc5e7c35ca6b8e713384cb364ac8ebc1026e7367
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-06-09 13:11:56 +0530

    jwt installed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
@@ -27,6 +28,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const danceCollection = client.db("summerSchool").collection("danceClasses");
+    const instructorCollection = client.db("summerSchool").collection("instructor");
+
+
+
+    // jwt related api
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign({
+        user
+      }, process.env.USER_TOKEN, { expiresIn: '1h' });
+
+    })
+    // dance class related api
+    app.get('/classes', async (req, res) => {
+      const result = await danceCollection.find().toArray();
+      res.send(result)
+    })
+
+    // instructor related api
+    app.get('/instructor', async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -53,3 +85,15 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`summer school live on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
